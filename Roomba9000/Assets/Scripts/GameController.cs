d058babe3049800d5bf36e3599375afdbe5e44ce
Daniel Schroeder <deadlydog@hotmail.com>
2019-01-26 16:02:30 -0600

    Merge DansScene into MainScene

--- Roomba9000/Assets/Scripts/GameController.cs
+++ Roomba9000/Assets/Scripts/GameController.cs
@@ -22,7 +22,7 @@ public class GameController : MonoBehaviour
 	{
 		for (int i = 0; i < numberOfPickUps; i++)
 		{
-			var position = new Vector3(Random.Range(-pickUpSpawnValues.x, pickUpSpawnValues.x), 0, Random.Range(-pickUpSpawnValues.z, pickUpSpawnValues.z));
+			var position = new Vector3(Random.Range(-pickUpSpawnValues.x, pickUpSpawnValues.x), pickUpSpawnValues.y, Random.Range(-pickUpSpawnValues.z, pickUpSpawnValues.z));
 			var rotation = Quaternion.identity;
 			Instantiate(pickUp, position, rotation);
 		}
