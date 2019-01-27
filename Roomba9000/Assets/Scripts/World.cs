c60e2b0d425e324e6b17f223c97081547b9ba20f
Laurie <laurieHardingRussell@gmail.com>
2019-01-26 21:00:43 -0600

    generated material.

--- Roomba9000/Assets/Scripts/World.cs
+++ Roomba9000/Assets/Scripts/World.cs
@@ -29,10 +29,16 @@ public class World : MonoBehaviour
     
     }
 
-    int[,] generateRoomBox(int size) {
+    void OnDrawGizmos() {
+        // Gizmos.color = Color.yellow;
+        // Gizmos.DrawSphere(transform.position, 0.5f);
+    }
+
+    private int[,] generateRoomBox(int size) {
         int startingCorner = (MAX_SIZE - size) / 2;
         int endCorner = startingCorner + size;
         Random random = new Random();
+
         int[,] map = new int[MAX_SIZE, MAX_SIZE]; 
         for (int x = startingCorner; x < endCorner +size; x++) {
             int type = CARPET; 
@@ -49,24 +55,20 @@ public class World : MonoBehaviour
         return map;
     }
 
-    void OnDrawGizmos() {
-        // Gizmos.color = Color.yellow;
-        // Gizmos.DrawSphere(transform.position, 0.5f);
-    }
-
-    void createMesh(int[,] map) {
+    private void createMesh(int[,] map) {
         Mesh mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
         List<Vector3> vertArray = new List<Vector3>();
+        List<Vector2> meshUV = new List<Vector2>();
         List<int> triangles = new List<int>();
         string output = "";
         if (map != null) {
             for (int x = 0; x < MAX_SIZE; x++) {
                 for (int z = 0; z < MAX_SIZE; z++) {
                     if (map[x,z] == CARPET) {
-                        generateSquare(x, z, vertArray, triangles);
+                        generateSquare(x, z, vertArray, triangles, meshUV);
                     } else if (map[x,z] == WALL) {
-                        generateSquare(x, z, vertArray, triangles);
+                        generateSquare(x, z, vertArray, triangles, meshUV);
                         // generateWall(x, z, vertArray, triangles);
                     }
                     output += map[x,z];
@@ -76,17 +78,24 @@ public class World : MonoBehaviour
 
             // Debug.Log(output);
         }
+        
+        Material material = GetComponent<Renderer>().material;  
+        Texture2D texture = generateTexture(50, Color.yellow);
+        material.mainTextureScale = new Vector2(0.02f, 0.02f);
+        material.mainTexture = texture;
 
         mesh.vertices = vertArray.ToArray();
+        mesh.uv = meshUV.ToArray();
         mesh.triangles = triangles.ToArray();
         mesh.name = "terrain"; 
 
         MeshCollider meshCollider = gameObject.AddComponent(typeof(MeshCollider)) as MeshCollider;
         mesh.RecalculateBounds();
         meshCollider.sharedMesh = mesh;
+        
     }
 
-    void generateSquare(int x, int z, List<Vector3> vertArray, List<int> triangles) {
+    private void generateSquare(int x, int z, List<Vector3> vertArray, List<int> triangles, List<Vector2> meshUV) {
         int start = vertArray.Count;
 
         float offset = (TILE_SIZE * MAX_SIZE /2);
@@ -102,9 +111,30 @@ public class World : MonoBehaviour
         triangles.Add(start);
         triangles.Add(start + 2);
         triangles.Add(start + 1);
+        meshUV.Add(new Vector2(0.0f, 0.0f));
+        meshUV.Add(new Vector2(1.0f, 0.0f)); 
+        meshUV.Add(new Vector2(1.0f, 1.0f));
+        meshUV.Add(new Vector2(0.0f, 1.0f)); 
 
         triangles.Add(start + 2);
         triangles.Add(start + 3);
         triangles.Add(start + 1);
+    }
+
+    private Texture2D generateTexture(int size, Color colour) {
+        Texture2D texture = new Texture2D(size, size, TextureFormat.ARGB32, false);
+
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                float red = Random.Range(0.0f, 0.5f);
+                float green = Random.Range(0.0f, 0.5f);
+                float blue = Random.Range(0.0f, 0.5f);
+                Color offsetColour = new Color(red, green, blue, 1.0f);
+                texture.SetPixel(0, 0, colour - offsetColour);
+            }
+        }
+    
+        texture.Apply();
+        return texture;
     }
 }
