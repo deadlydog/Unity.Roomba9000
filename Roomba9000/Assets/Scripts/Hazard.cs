cf542cae4f349f6af4bd054732c9ca3dcec8ddae
Daniel Schroeder <deadlydog@hotmail.com>
2019-01-27 13:20:40 -0600

    Make poo rotate

--- Roomba9000/Assets/Scripts/Hazard.cs
+++ Roomba9000/Assets/Scripts/Hazard.cs
@@ -5,4 +5,12 @@ using UnityEngine;
 public class Hazard : MonoBehaviour
 {
 	public int energyDrain;
+	private float rotationSpeed = 100.0f;
+
+	private void Update()
+	{
+		float rotateAmount = rotationSpeed * Time.deltaTime;
+		Vector3 rotation = new Vector3(0, rotateAmount, 0);
+		transform.Rotate(rotation);
+	}
 }
