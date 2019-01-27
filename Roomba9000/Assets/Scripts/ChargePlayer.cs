58fb44ea149bc4dae7e8c7b35fac8547ab665423
Daniel Schroeder <deadlydog@hotmail.com>
2019-01-27 13:53:33 -0600

    Add collider to charger and get charging working

--- Roomba9000/Assets/Scripts/ChargePlayer.cs
+++ Roomba9000/Assets/Scripts/ChargePlayer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ChargePlayer : MonoBehaviour
 {
-    const float CHARGING_RATE = 1;
+    const float CHARGING_RATE = 5;
 
     private GameController gameController;
 
